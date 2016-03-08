0fddba91df32a6884e5407ded154ff4299a08b4a
galbraun <galbraun@gmail.com>
2016-03-08 22:36:52 +0200

    with authentication. Successful format. not checked with linux.

--- sata/sata_main.c
+++ sata/sata_main.c
@@ -28,23 +28,231 @@ volatile UINT32		g_sata_action_flags;
 
 
 /****************** GAL AND SHANI PROJECT ******************/
-const int MAX_AES_SIZE = 64;
-const int MAX_MSG_SIZE = 128;
-char msgToUser[128] = {0};
+#define MAX_MSG_SIZE 128
+char msgToUser[MAX_MSG_SIZE] = {0};
 int  commandCounter = 0;
 
 ESystemState		 currSysState  = SYS_INITIAL_STATE;
 EAuthenticationState currAuthState = AUTH_INITIAL_STATE; // after an authentication process began - sign in which step in the process 
 EMsgToUserState		 currMsgState  = MSG_INVALID_STATE;
 
-#define get_num_bank(lpn)             ((lpn) % NUM_BANKS)
-#define get_lpn(bank, page_num)       (g_misc_meta[bank].lpn_list_of_cur_vblock[page_num])
-#define CHECK_LPAGE(lpn)              ASSERT((lpn) < NUM_LPAGES)
+#define get_num_bank(lpn)       ((lpn) % NUM_BANKS)
+#define get_lpn(bank, page_num) (g_misc_meta[bank].lpn_list_of_cur_vblock[page_num])
+#define CHECK_LPAGE(lpn)        ASSERT((lpn) < NUM_LPAGES)
+
+
+// Get data form RAM
+void getData(int lba, char* msg, int msgMaxSize)
+{
+	UINT32 sect_offset  = lba % SECTORS_PER_PAGE;
+	msg[0] = (char) read_dram_8 (WR_BUF_PTR(g_ftl_write_buf_id) + (sect_offset*BYTES_PER_SECTOR));
+	
+	for (int i = 1; (i < msgMaxSize) && (i < BYTES_PER_SECTOR) && (msg[i-1] != 0); i++) {
+		msg[i] = (char) read_dram_8 (WR_BUF_PTR(g_ftl_write_buf_id) + (sect_offset*BYTES_PER_SECTOR) + i);
+	}
+	
+	msg[msgMaxSize-1] = 0; //msg should be null terminated
+	
+	g_ftl_write_buf_id = (g_ftl_write_buf_id + 1) % NUM_WR_BUFFERS;
+}
+
+// Send data to RAM
+void sendData(char* msg, int msgSize)
+{
+	UINT32 next_read_buf_id = (g_ftl_read_buf_id + 1) % NUM_RD_BUFFERS;
+				
+	while (next_read_buf_id == GETREG(SATA_RBUF_PTR));
+	
+	for (int k = 0; k < msgSize; k++){
+		write_dram_8(RD_BUF_PTR(g_ftl_read_buf_id) + (36 * BYTES_PER_SECTOR) + k, msg[k]);
+	}
+
+	SETREG(SATA_RBUF_PTR, g_ftl_read_buf_id);	// change sata_read_ptr
+	SETREG(BM_STACK_RDSET, next_read_buf_id);	// change bm_read_limit
+	SETREG(BM_STACK_RESET, 0x02);				// change bm_read_limit
+		
+	g_ftl_read_buf_id = next_read_buf_id;
+}
+
+void decryptBuffer(uint8_t* result, const char* data, const uint8_t* key)
+{	
+	uint8_t iv[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+	
+	uint8_t input[64]	= {0};
+	int dataSize = strlen(data);
+	strncpy((char*)input, data, (dataSize < 64 ? dataSize : 64));
+	
+	AES128_CBC_decrypt_buffer(result + 0,  input+0,  16, key, iv);
+	AES128_CBC_decrypt_buffer(result + 16, input+16, 16, 0, 0);
+	AES128_CBC_decrypt_buffer(result + 32, input+32, 16, 0, 0);
+	AES128_CBC_decrypt_buffer(result + 48, input+48, 16, 0, 0);
+}
+
+void encryptBuffer(uint8_t* result, uint8_t* data, uint32_t length, const uint8_t* key)
+{	
+	uint8_t iv[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+	
+	AES128_CBC_encrypt_buffer(result, data, length, key, iv);
+}
+
+void authentication(CMD_T cmd){
+	
+	char* password = "testtest"; // TODO: this is for test - need to pull from passwordfile by userName
+	char* userName = NULL;
+
+	uint8_t  result[64]	= {0};
+	uint8_t* dataPart1 = NULL;
+	uint8_t* dataPart2 = NULL;
+	uint8_t* hashedPass_server = NULL;
+	
+	static uint64_t server_random	= 0;
+	static uint64_t server_public 	= 0;
+	static uint64_t server_symetricKey 	= 0;
+	static uint32_t server_Challenge	= 0;
+	static uint32_t server_ChallengeAck = 0;
+	static uint32_t user_Challenge		= 0;
+	
+	switch(currAuthState)
+	{
+	case AUTH_INITIAL_STATE: //expect to receive user pablic key
+		if(cmd.cmd_type == READ){ // Not Good
+			if(cmd.lba == 102){
+				char msg[MAX_MSG_SIZE] = "stam";
+				sendData(msg, strlen(msg)+1);
+				uart_printf("XXX massage to user: %s", msg);
+			}
+			else{
+				char msg[MAX_MSG_SIZE] = "SORRY, NOT AUTHENTICATED!";
+				if(currSysState != SYS_INITIAL_STATE) sendData(msg, strlen(msg)+1);
+				uart_printf("Problem: State 0, cmd = R, lba = %d", cmd.lba);
+			}
+		}
+		if(cmd.cmd_type == WRITE && cmd.lba == 102) { //OK: get user key and do actions
+			uart_printf("AUTH_INITIAL_STATE: ");
+			char msg[BYTES_PER_SECTOR] = {0};
+			getData(cmd.lba, msg, BYTES_PER_SECTOR);
+			
+			userName = msg;
+			dataPart1 = (uint8_t*) strchr(msg,'|') + 1;
+			*strchr(msg,'|') = 0; //so userName will be null terminated string
+			uart_printf("user name is: %s", userName);
+			
+			hashedPass_server = md5Hash(password, strlen(password));
+			decryptBuffer(result, dataPart1, hashedPass_server);
+			
+			uint64_t user_public = 0;
+			memcpy(&user_public, result, sizeof(uint64_t));
+			
+			uart_printf("user public is: %lld", user_public);
+			
+			server_random = randomint64();				
+			server_symetricKey = powmodp(user_public, server_random);
+			
+			server_Challenge = randomint32();
+			encryptBuffer(msgToUser, (uint8_t*) &server_Challenge, sizeof(server_Challenge), &server_symetricKey);
+			
+			server_public = powmodp(G, server_random);
+			encryptBuffer(msgToUser+64, &server_public, sizeof(server_public), &hashedPass_server);
+			
+			free(hashedPass_server); //allocated by md5Hash 
+			
+			currMsgState  = MSG_READY;
+			currAuthState = AUTH_GOT_USER_KEY;
+			
+			uart_printf("server public is: %lld", server_public);
+			//uart_printf("massage from user: %s", msg);
+		}
+		break;
+		
+	case AUTH_GOT_USER_KEY: //expect to send server key and challenge
+		if(cmd.cmd_type == READ && cmd.lba == 96) { //OK: send server key and challenge
+			//char msg[MAX_MSG_SIZE] = "Good! In State 1!";
+			//sendData(msg, strlen(msg)+1);
+			//uart_printf("massage to user: %s", msg);
+			if(currMsgState != MSG_READY){
+				strncpy(msgToUser, "Msg is not ready! ..?", 21);
+			}
+			sendData(msgToUser, strlen(msgToUser)+1);
+			currMsgState = MSG_INVALID_STATE;
+			memset(msgToUser, 0, MAX_MSG_SIZE);
+			currAuthState = AUTH_SERVER_KEY_CHALLANGE_WAS_SENT;
+		}
+		if(cmd.cmd_type == WRITE){ // Not Good
+			uart_printf("PROBLEM: State 1, cmd = W, lba = %d", cmd.lba);
+			// TODO: print error?
+			currAuthState = AUTH_INITIAL_STATE;
+		}
+		break;
+		
+	case AUTH_SERVER_KEY_CHALLANGE_WAS_SENT: //expect to receive server challenge ake and user challenge
+		if(cmd.cmd_type == READ){ // Not Good
+			if(cmd.lba == 102){
+				char msg[MAX_MSG_SIZE] = "stam";
+				sendData(msg, strlen(msg)+1);
+				uart_printf("XXX massage to user: %s", msg);
+			}
+			else{
+				char msg[MAX_MSG_SIZE] = "SORRY, NOT AUTHENTICATED!";
+				if(currSysState != SYS_INITIAL_STATE) sendData(msg, strlen(msg)+1);
+				uart_printf("PROBLEM: State 2, cmd = R, lba = %d", cmd.lba);
+				currAuthState = AUTH_INITIAL_STATE;
+			}
+		}
+		if(cmd.cmd_type == WRITE && cmd.lba == 102) { // OK: get server challenge ake and user challenge
+			uart_printf("AUTH_SERVER_KEY_CHALLANGE_WAS_SENT: ");
+			char msg[BYTES_PER_SECTOR] = {0};
+			getData(cmd.lba, msg, BYTES_PER_SECTOR);
+			
+			dataPart1 = (uint8_t*) msg; //encrypted server_ChallengeAck
+			dataPart2 = (uint8_t*) + 64; // encrypted userChallenge
+			//*strchr(msg,'|') = 0; //so dataPart1 will be null terminated string
+			
+			decryptBuffer(result, dataPart1, server_symetricKey); //TODO: should be &server_symetricKey ?
+			memcpy(&server_ChallengeAck, result, sizeof(uint32_t));
+			
+			uart_printf("server challenge: %ld", server_Challenge);
+			uart_printf("server challenge Ake: %ld", server_ChallengeAck);
+			
+			decryptBuffer(result, dataPart2, server_symetricKey); //TODO: should be &server_symetricKey ?
+			memcpy(&user_Challenge, result, sizeof(uint32_t));
+			
+			uart_printf("user challenge: %ld", user_Challenge);
+			
+			// TODO: do some manipulation of user_Challenge (xor with symetric key? ) and send it to user
+			//encryptBuffer(msgToUser, &user_Challenge, sizeof(user_Challenge), &server_symetricKey);
+			int dataSize = 64; //strlen((char*)dataPart2);
+			memcpy(msgToUser, dataPart2, ( dataSize < MAX_MSG_SIZE ? dataSize : MAX_MSG_SIZE));
+			currMsgState  = MSG_READY;
+			currAuthState = AUTH_GOT_USER_CHALLENGE;	
+			//uart_printf("massage from user: %s", msg);			
+		}
+		break;
+		
+	case AUTH_GOT_USER_CHALLENGE: // expect to send user challenge ack
+		if(cmd.cmd_type == READ && cmd.lba == 96) { // OK: send user challenge ack
+			//char msg[MAX_MSG_SIZE] = "Good! In State 3!";
+			//sendData(msg, strlen(msg)+1);
+			//uart_printf("massage to user: %s", msg);
+			if(currMsgState != MSG_READY){
+				strncpy(msgToUser, "Msg is not ready! ..?", 21);
+			}
+			sendData(msgToUser, strlen(msgToUser)+1);
+			currMsgState = MSG_INVALID_STATE;
+			memset(msgToUser, 0, MAX_MSG_SIZE);
+			currAuthState = AUTH_AUTHENTICATION_FINISHED;
+			currSysState  = SYS_AUTHENTICATED;		
+		}
+		if(cmd.cmd_type == WRITE){ // Not Good
+			uart_printf("PROBLEM: State 3, cmd = R, lba = %d", cmd.lba);
+			// TODO: print error
+			currAuthState = AUTH_INITIAL_STATE;
+		}
+		break;
+	}
+}
 
 /****************** GAL AND SHANI PROJECT ******************/
 /****************** END OF CURRENT CHANGE ******************/
-
-
 
 
 static UINT32 eventq_get_count(void)
@@ -98,113 +306,6 @@ __inline ATA_FUNCTION_T search_ata_function(UINT32 command_code)
 	return ata_function;
 }
 
-
-/****************** GAL AND SHANI PROJECT ******************/
-
-// Get data form RAM
-void getData(int lba, char* msg, int msgMaxSize)
-{
-	UINT32 sect_offset  = lba % SECTORS_PER_PAGE;
-	int i = 0;
-	for (i = 0; i < BYTES_PER_SECTOR; i++) {
-		char ch = (char) read_dram_8(WR_BUF_PTR(g_ftl_write_buf_id) + (sect_offset*BYTES_PER_SECTOR) + i);
-		if (ch == 0) {
-			break;
-		}
-		msg[i] = ch;
-	}
-	msg[i+1] = '\0';		
-	g_ftl_write_buf_id = (g_ftl_write_buf_id + 1) % NUM_WR_BUFFERS;
-	uart_printf("** msg from user is: %s", msg);
-	//UINT32 sect_offset  = lba % SECTORS_PER_PAGE;
-	//msg[0] = (char) read_dram_8 (WR_BUF_PTR(g_ftl_write_buf_id) + (sect_offset*BYTES_PER_SECTOR));
-	
-	//for (int i = 1; (i < BYTES_PER_SECTOR) && (msg[i-1] != 0); i++) {
-	//	msg[i] = (char) read_dram_8 (WR_BUF_PTR(g_ftl_write_buf_id) + (sect_offset*BYTES_PER_SECTOR) + i);
-	//}
-	
-	//msg[msgMaxSize-1] = 0; //msg should be null terminated
-}
-
-// Send data to RAM
-void sendData(char* msg, int msgSize)
-{
-	UINT32 next_read_buf_id = (g_ftl_read_buf_id + 1) % NUM_RD_BUFFERS;
-				
-	while (next_read_buf_id == GETREG(SATA_RBUF_PTR));
-	
-	for (int k = 0; k < msgSize; k++){
-		write_dram_8(RD_BUF_PTR(g_ftl_read_buf_id) + (36 * BYTES_PER_SECTOR) + k, msg[k]);
-	}
-
-	SETREG(SATA_RBUF_PTR, g_ftl_read_buf_id);	// change sata_read_ptr
-	SETREG(BM_STACK_RDSET, next_read_buf_id);	// change bm_read_limit
-	SETREG(BM_STACK_RESET, 0x02);				// change bm_read_limit
-		
-	g_ftl_read_buf_id = next_read_buf_id;
-}
-
-void decryptBuffer(uint8_t* result, uint8_t* input, uint32_t length, const uint8_t* key, const uint8_t* iv)
-{	
-	if(length ==  MAX_AES_SIZE){
-		AES128_CBC_decrypt_buffer(result + 0,  input+0,  16, key, iv);
-		AES128_CBC_decrypt_buffer(result + 16, input+16, 16, 0, 0);
-		AES128_CBC_decrypt_buffer(result + 32, input+32, 16, 0, 0);
-		AES128_CBC_decrypt_buffer(result + 48, input+48, 16, 0, 0);
-	}
-}
-
-void authentication(CMD_T cmd){
-	switch(currAuthState)
-	{
-	case AUTH_INITIAL_STATE: //expect to receive user pablic key
-		if(cmd.cmd_type == READ){
-			//send to user: not authenticated
-		}
-		if(cmd.cmd_type == WRITE && cmd.lba == 100) {
-			//get user key and do actions
-			currAuthState = AUTH_GOT_USER_KEY;			
-		}
-		break;
-		
-	case AUTH_GOT_USER_KEY: //expect to send server key and challenge
-		if(cmd.cmd_type == READ && cmd.lba == 96) {
-			//send server key and challenge
-			currAuthState = AUTH_SERVER_KEY_CHALLANGE_WAS_SENT;			
-		}
-		if(cmd.cmd_type == WRITE){
-			// TODO: print error
-			currAuthState = AUTH_INITIAL_STATE;
-		}
-		break;
-		
-	case AUTH_SERVER_KEY_CHALLANGE_WAS_SENT: //expect to receive server challenge ake and user challenge
-		if(cmd.cmd_type == READ){
-			//send to user: not authenticated
-			currAuthState = AUTH_INITIAL_STATE;
-		}
-		if(cmd.cmd_type == WRITE && cmd.lba == 100) {
-			//get server challenge ake and user challenge
-			currAuthState = AUTH_GOT_USER_CHALLENGE;			
-		}
-		break;
-		
-	case AUTH_GOT_USER_CHALLENGE: //expect to send user challenge ack
-		if(cmd.cmd_type == READ && cmd.lba == 96) {
-			//send user challenge ack
-			currAuthState = AUTH_AUTHENTICATION_FINISHED;
-			currSysState  = SYS_AUTHENTICATED;		
-		}
-		if(cmd.cmd_type == WRITE){
-			// TODO: print error
-			currAuthState = AUTH_INITIAL_STATE;
-		}
-		break;
-	}
-}
-/****************** GAL AND SHANI PROJECT ******************/
-/****************** END OF CURRENT CHANGE ******************/
-
 void Main(void)
 {
 	while (1)
@@ -212,10 +313,23 @@ void Main(void)
 		if (eventq_get_count())
 		{
 			CMD_T cmd;
+
 			eventq_get(&cmd);
+
+			/*if (cmd.cmd_type == READ)
+			{
+				ftl_read(cmd.lba, cmd.sector_count);
+			}
+			else
+			{
+				ftl_write(cmd.lba, cmd.sector_count);
+			}*/
 			
-			currSysState = (commandCounter++ < 5000 ? SYS_INITIAL_STATE : SYS_NOT_AUTHENTICATED);
-		
+			if(currSysState == SYS_INITIAL_STATE)
+				currSysState = (commandCounter++ < 5000 ? SYS_INITIAL_STATE : SYS_NOT_AUTHENTICATED);
+			
+			//if(commandCounter % 50 == 0)
+				//uart_printf("%d: type = %s, lba = %d", commandCounter, (cmd.cmd_type == READ ? "R" : "W"), cmd.lba);
 			//#TODO: change currSystemState 
 			
 			if(currSysState == SYS_AUTHENTICATED){
@@ -229,26 +343,29 @@ void Main(void)
 				}
 			}
 			else {
-				authentication(cmd);
+				if (commandCounter > 2750){
+					uart_printf("%d: before: AuthState = %d", commandCounter, currAuthState);
+					authentication(cmd);
+					uart_printf("after: AuthState = %d", currAuthState);
+				} 
 				if(currSysState == SYS_INITIAL_STATE){
-					if (cmd.cmd_type == READ) {
+					if (commandCounter > 2750)uart_printf("*** %d: SYS_INITIAL_STATE", commandCounter);
+					if (cmd.cmd_type == READ && cmd.lba != 96 && cmd.lba != 102) {
 						ftl_read(cmd.lba, cmd.sector_count);
 					}
-					else {
+					else if (cmd.cmd_type == WRITE && cmd.lba != 102){
 						ftl_write(cmd.lba, cmd.sector_count);
 					}
 				}
 			}
-			/*
 			
-			
-			
-			if (cmd.cmd_type == READ)
+			/*if (cmd.cmd_type == READ)
 			{
 				if(cmd.cmd_type == READ && cmd.lba == 96){
-					char msg[128] = "hi hi shani";
+					char msg[MAX_MSG_SIZE] = "  hi shani";
+					msg[0] = (++commandCounter)%10 + '0';
 					sendData(msg, strlen(msg)+1);
-					uart_printf("massage to user: %s", msg);
+					uart_printf("massage to user: %s", msg);					
 				}
 				else{
 					ftl_read(cmd.lba, cmd.sector_count);
@@ -256,7 +373,7 @@ void Main(void)
 			}
 			else
 			{
-				if(cmd.cmd_type == WRITE && cmd.lba == 100){
+				if(cmd.cmd_type == WRITE && cmd.lba == 102){
 					char msg[BYTES_PER_SECTOR] = {0};
 					getData(cmd.lba, msg, BYTES_PER_SECTOR);
 					uart_printf("massage from user: %s", msg);
@@ -264,8 +381,7 @@ void Main(void)
 				else{
 					ftl_write(cmd.lba, cmd.sector_count);
 				}
-			}
-			*/
+			}*/
 			
 		}
 		else if (g_sata_context.slow_cmd.status == SLOW_CMD_STATUS_PENDING)
